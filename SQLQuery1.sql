8e00253f440388558ca340b44306fb401bc14f34
Ilya <murashkevich.ilya@mail.ru>
2020-12-15 12:21:56 +0300

    Update SQL script

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,4 @@
-/*ALTER TABLE EmployeeTask
+ALTER TABLE EmployeeTask
 DROP CONSTRAINT FK_EmployeeTask_Employee,
 	 CONSTRAINT FK_EmployeeTask_Tasks;
 	 
@@ -59,24 +59,23 @@ ALTER TABLE Image
 	ADD CONSTRAINT FK_Image_Employee FOREIGN KEY (EmployeeId) REFERENCES Employee ON DELETE CASCADE;
 
 GO
-*/
+
 DROP TABLE Person
 GO
 CREATE TABLE Person
 (Id int identity primary key,
  Login varchar(50) NOT NULL unique,
- Password varchar(50) NOT NULL/*,
- Role varchar(50) DEFAULT 'user'*/
+ Password varchar(50) NOT NULL
 )
 GO
-/*
+
 INSERT INTO Employee
-	VALUES/*('Иванов Иван Иванович',22,'ул. Пушкина 20','Программист'), 
+	VALUES('Иванов Иван Иванович',22,'ул. Пушкина 20','Программист'), 
 	('Азявчиков Сегрей Сергеевич',31,'ул. Минская 15','Уборщик'), 
-	('Кириллов Кирилл Сергеевич',18,'ул. Бобруйская 6','Работник'), */
+	('Кириллов Кирилл Сергеевич',18,'ул. Бобруйская 6','Работник'), 
 	('Петров Александр Сергеевич',21,'ул. Советская 10','Worker');
-GO*/
-/*
+GO
+
 INSERT INTO Tasks
 	VALUES('Работать','Зарабатывать баблишко'), 
 	('Работать2','Зарабатывать баблишко2');
@@ -87,19 +86,19 @@ INSERT INTO EmployeeTask
 	(1,2,'2020-10-09',3), 
 	(3,1,'2013-06-06',5);
 GO
-*/
+
 INSERT INTO Person
 	VALUES('admin@gmail.com','12345'), 
 	('qwerty@gmail.com','55555'), 
 	('qwqw@mail.ru','111');
 GO
-/*
+
 INSERT INTO Image
-	VALUES('имя1',,1), 
-	('имя2',,2), 
-	('имя3',,3), 
-	('имя4',,4);
-GO*/
+	VALUES('имя1','',1), 
+	('имя2','',2), 
+	('имя3','',3), 
+	('имя4','',4);
+GO
 SELECT * from Tasks
 GO
 SELECT * from Employee
@@ -110,7 +109,7 @@ SELECT * from EmployeeTask
 GO
 
 /*SELECT * from EmployeeTask
-GO*//*
+GO
 Select emp.Id, ts.Id, emp.Name, ts.Name, ts.Description, AppointmentDate, Estemate
 FROM EmployeeTask as empTask INNER JOIN Employee as emp ON emp.Id = empTask.EmployeeId
 INNER JOIN Tasks as ts ON ts.Id = empTask.TaskId
@@ -126,11 +125,11 @@ Select ts.Name, ts.Description FROM Tasks as ts
 INNER JOIN EmployeeTask as empTasks ON ts.Id = empTasks.TaskId
 WHERE EmployeeId IN (Select Id from Employee)
 GO
-Select * from Image*/
+Select * from Image
 
-/*Select emp.Id, emp.Name FROM Employee as emp INNER JOIN EmployeeTask 
-as empTask ON emp.id = empTask.EmployeeId */
-/*WHERE empTask.TaskId = 2*/
-/*
+Select emp.Id, emp.Name FROM Employee as emp INNER JOIN EmployeeTask 
+as empTask ON emp.id = empTask.EmployeeId 
+WHERE empTask.TaskId = 2
+
 Select emp.Name, ts.Name, ts.Description FROM Employee as emp, Task as ts
 WHERE emp.Id IN (SELECT EmployeeId FROM EmployeeTask) AND ts.Id IN (SELECT TaskId FROM EmployeeTask)*/
